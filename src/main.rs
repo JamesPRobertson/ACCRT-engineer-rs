3ad9c662794486e30cd17e98810522f07fe76a6f
James Robertson <jamespr.0312@gmail.com>
2022-08-08 23:23:48 -0500

    Move into constructor

--- src/main.rs
+++ src/main.rs
@@ -15,10 +15,21 @@ const LISTEN_IP_ADDR_PORT: &str = "0.0.0.0:9001";
 const POLLING_RATE_IN_MS: u64 = 16; // Roughly 60 Hz
 
 struct NetworkInfo {
-    socket: std::net::UdpSocket,
+    socket:    std::net::UdpSocket,
     server_ip: String,
     listen_ip: String,
     heartbeat: std::time::SystemTime
+}
+
+impl NetworkInfo {
+    fn new(server_ip: String) -> NetworkInfo {
+        NetworkInfo {
+            socket: std::net::UdpSocket::bind(&server_ip).unwrap(),
+            server_ip,
+            listen_ip: String::from(LISTEN_IP_ADDR_PORT),
+            heartbeat: std::time::SystemTime::now()
+        }
+    }
 }
 
 struct TelemetryParser {
@@ -31,34 +42,10 @@ struct TelemetryParser {
 
 impl TelemetryParser {
     fn main(&mut self) {
-
-        // CONSTRUCTION
-        let server_ip_addr = match get_ip_from_args() {
-            Some(val) => val,
-            None => {
-                println!("Failed to supply server IP as argument. Exiting...");
-                std::process::exit(1);
-            }
-        };
-
-        println!("Binding to socket with {}...", LISTEN_IP_ADDR_PORT);
-        self.network.socket = std::net::UdpSocket::bind(LISTEN_IP_ADDR_PORT).unwrap();
-
-        println!("Sending request for data to {}", &server_ip_addr);
-        match self.network.socket.send_to("Give me the data!".as_bytes(), &server_ip_addr) {
-            Ok(_size) => { },
-            Err(_e) => panic!("Send request for data failed!")
-        };
-
-        self.wait_for_initial_message();
-        // END CONSTRUCTION
-
-        terminal_setup(); // From this point on we are in the alternate buffer
-
+        // TODO this will have to be rethought with asyncness
         let mut hotkeys: HashMap<event::Event, fn()> = HashMap::new();
         hotkeys.insert(self.build_key_event('q'), exit_terminal);
-
-        let mut heartbeat = std::time::SystemTime::now();
+        //
         
         let mut blocks: Vec<Box<dyn TUIBlock>> = vec![
             Box::new(tui_blocks::Tachometer::new(0,0)),
@@ -86,6 +73,7 @@ impl TelemetryParser {
 
             println!("{}", terminal::Clear(terminal::ClearType::All));
 
+            // TODO instead of this, we need to know when we are actually getting good data
             if self.physics["packetId"] != 0 {
                 if !static_data_initialized {
                     self.init_vector_statics(&mut blocks);
@@ -99,13 +87,33 @@ impl TelemetryParser {
             }
             else {
                 println!("{}{}", terminal::Clear(terminal::ClearType::All) ,cursor::MoveTo(0,0));
-                println!("Connection established to {}, waiting for data...", server_ip_addr);
+                println!("Connection established to {}, waiting for data...", self.network.server_ip);
                 static_data_initialized = false;
             }
 
-            heartbeat = self.send_heartbeat_to_server(&server_ip_addr, heartbeat);
+            self.send_heartbeat_to_server();
             sleep_for_polling_rate();
         }
+    }
+
+    fn new(server_ip_addr: String) -> TelemetryParser {
+        return TelemetryParser {
+            physics: serde_json::Value::Null,
+            graphics: serde_json::Value::Null,
+            statics: serde_json::Value::Null,
+            network: NetworkInfo::new(server_ip_addr)
+        }
+    }
+
+    fn preconnect_setup(&self) {
+        println!("Sending request for data to {}", &self.network.server_ip);
+
+        match self.network.socket.send_to("Give me the data!".as_bytes(), &self.network.server_ip) {
+            Ok(_size) => { },
+            Err(_e) => panic!("Send request for data failed!")
+        };
+
+        self.wait_for_initial_message();
     }
 
     fn update_telemetry_from_connection(&mut self) -> Result<(), serde_json::Error>{
@@ -125,18 +133,13 @@ impl TelemetryParser {
         Ok(())
     }
 
-    fn send_heartbeat_to_server(&mut self,
-                                ip_addr: &String,
-                                heartbeat: std::time::SystemTime) -> std::time::SystemTime {
+    fn send_heartbeat_to_server(&mut self){
         let current_time = std::time::SystemTime::now();
-        let mut new_heartbeat = heartbeat;
-
-        if current_time.duration_since(heartbeat).unwrap() > HEARTBEAT_DELTA_IN_MS {
-            self.network.socket.send_to("I'm alive!".as_bytes(), ip_addr).unwrap();
-            new_heartbeat = current_time;
+        
+        if current_time.duration_since(self.network.heartbeat).unwrap() > HEARTBEAT_DELTA_IN_MS {
+            self.network.socket.send_to("I'm alive!".as_bytes(), &self.network.server_ip).unwrap();
+            self.network.heartbeat = current_time;
         }
-
-        return new_heartbeat;
     }
 
     fn init_vector_statics(&self, blocks: &mut Vec<Box<dyn TUIBlock>>) {
@@ -145,13 +148,13 @@ impl TelemetryParser {
         }
     }
 
-    fn wait_for_initial_message(&mut self) {
+    fn wait_for_initial_message(&self) {
         let mut buffer = [0; BUFFER_SIZE];
         println!("Waiting for connection...");
         self.network.socket.recv(&mut buffer).unwrap();
-        println!("Connection successful!")
+        println!("Connection successful!");
+        // TODO we may have to update heartbeat, we may not
     }
-
 
     fn is_event_available(&self) -> bool {
         event::poll(std::time::Duration::from_millis(0)).unwrap()
@@ -166,9 +169,20 @@ impl TelemetryParser {
 }
 
 fn main() {
-    println!("Hello, World!");
-}
+    let server_ip_addr = match get_ip_from_args() {
+        Some(val) => val,
+        None => {
+            println!("Failed to supply server IP as argument. Exiting...");
+            std::process::exit(1);
+        }
+    };
 
+    let telemetry_parser = TelemetryParser::new(server_ip_addr);
+
+    telemetry_parser.preconnect_setup();
+
+    terminal_setup();
+}
 
 fn get_ip_from_args() -> Option<String> {
     let args: Vec<String> = std::env::args().collect();
@@ -181,7 +195,6 @@ fn get_ip_from_args() -> Option<String> {
     }
 }
 
-
 fn terminal_setup() {
     crossterm::terminal::enable_raw_mode().unwrap();
     crossterm::execute!(std::io::stdout(), crossterm::terminal::EnterAlternateScreen).unwrap();
@@ -191,7 +204,6 @@ fn terminal_cleanup() {
     crossterm::execute!(std::io::stdout(), crossterm::terminal::LeaveAlternateScreen).unwrap();
     crossterm::terminal::disable_raw_mode().unwrap();
 }
-
 
 fn exit_terminal() {
     terminal_cleanup();
